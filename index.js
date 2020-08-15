dea7586822f5d2c588e120a4858b1165df6ff0fd
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-15 23:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const append = ['Milo', 'Otis', 'Garfield']
 
 const prepend = ['Milo', 'Otis', 'Garfield'] 
 
-const removeLast = []
+const removeLast = ['Milo', 'Otis', 'Garfield']
 
 const removeFirst = []
